a6c682fb8fc04dc2e023ec190a728a121f303c14
servairmarketing <servairmarketing@gmail.com>
2026-03-10 17:54:49 +0000

    Fix coroutine scope: use SupervisorJob+IO instead of MainScope to prevent cancellation crashes

--- android/app/src/main/kotlin/com/compleat/compleat_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/compleat/compleat_mobile/MainActivity.kt
@@ -3,10 +3,12 @@ package com.compleat.compleat_mobile
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 
 class MainActivity : FlutterActivity() {
-    private val scope = MainScope()
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
